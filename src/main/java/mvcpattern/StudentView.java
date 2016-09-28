2bb2036efb8ba49f9694f047311c899273d9c0c5
runningkay <prodigla0906@gmail.com>
2016-09-28 10:34:48 +0800

    add mvc pattern

---
+++ src/main/java/mvcpattern/StudentView.java
@@ -0,0 +1,12 @@
+package mvcpattern;
+
+/**
+ * Created by Nozomi on 2016/9/28.
+ */
+public class StudentView {
+    public void printStudentDetails(String studentName,String studentRollNo){
+        System.out.println("Student: ");
+        System.out.println("Name: " + studentName);
+        System.out.println("Roll No: " + studentRollNo);
+    }
+}
